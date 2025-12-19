1dd25799c90cfa3a303311b98c1a2f5454478c34
Praveen K T <81749009+praveendon@users.noreply.github.com>
2025-12-20 00:22:52 +0530

    Add Navbar component with basic styling

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav
+      style={{
+        padding: "14px 20px",
+        borderBottom: "1px solid #e5e7eb",
+        fontWeight: 600,
+        fontSize: "18px",
+      }}
+    >
+      BiTech Care
+    </nav>
+  );
+}
